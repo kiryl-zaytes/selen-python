2f877720cda1903b7615b98c143f046b1b280c3e
kirilzayets <kiril.zayets@gmail.com>
2014-01-11 16:22:21 +0300

    report eff fe

--- gmail/grid/entry_point.py
+++ gmail/grid/entry_point.py
@@ -4,5 +4,5 @@ __author__ = 'kiryl_zayets'
 
 current_date = datetime.datetime.now().isoformat()
 report_name = '{0}report/{1}{2}'.format('--xunit-file=',str(current_date),'.xml')
-noseargs = ['foo', '-v','--with-xunit', report_name, '--processes=3','--process-timeout=40' ,'--tests=tests/face_page.py']
+noseargs = ['foo', '-v','--with-xunit', '--processes=3','--process-timeout=40' ,'--tests=tests/face_page.py']
 nose.run(argv=noseargs)
